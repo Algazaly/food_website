b29aced15965b8468c803a45cfcd4381bff4f955
unknown <lawalgazalysufyan@gmail.com>
2025-07-03 19:38:53 +0100

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,215 @@
+import basket_icon from './basket_icon.png'
+import search_icon from './search_icon.png'
+import Tomato from './Tomato.jpeg'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.jpeg'
+import food_1 from './food_1.jpeg'
+import food_2 from './food_2.jpeg'
+import food_3 from './food_3.jpeg'
+import food_4 from './food_4.jpeg'
+import food_5 from './food_5.jpeg'
+import food_6 from './food_6.jpeg'
+import food_7 from './food_7.jpeg'
+import food_8 from './food_8.jpeg'
+import food_9 from './food_9.jpeg'
+import food_10 from './food_10.jpeg'
+import food_11 from './food_11.jpeg'
+import food_12 from './food_12.jpeg'
+import food_13 from './food_13.jpeg'
+import food_14 from './food_14.jpeg'
+import food_15 from './food_15.jpeg'
+import food_16 from './food_16.jpeg'
+
+
+
+
+export const assets = {
+    basket_icon,
+    search_icon,
+    Tomato,
+
+}
+
+
+
+
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_2
+
+    },
+    {
+        menu_name: "Deserts",
+        menu_image: menu_3
+
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu_4
+
+    },
+    {
+        menu_name: "Coke",
+        menu_image: menu_5
+
+    },
+    {
+        menu_name: "Pur Veg",
+        menu_image: menu_6
+
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_8
+
+    }]
+
+    export const food_list = [
+        {
+            _id: "1",
+            name: "Greek salad",
+            image: food_1,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Salad"
+        },
+         {
+            _id: "2",
+            name: "Veg salad",
+            image: food_2,
+            price: 18,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Salad"
+        },
+        {
+            _id: "3",
+            name: "Clover salad",
+            image: food_3,
+            price: 16,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Salad"
+        },
+        {
+            _id: "4",
+            name: "Chicken salad",
+            image: food_4,
+            price: 24,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Salad"
+        },
+        {
+            _id: "5",
+            name: "Lasagna Rolls",
+            image: food_5,
+            price: 14,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Rolls"
+        },
+        {
+            _id: "6",
+            name: "Peri Peri Rolls",
+            image: food_6,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Rolls"
+        },
+        {
+            _id: "7",
+            name: "Chicken Rolls",
+            image: food_7,
+            price: 20,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Rolls"
+        },
+        {
+            _id: "8",
+            name: "Veg Rolls",
+            image: food_8,
+            price: 15,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Rolls"
+        },
+        {
+            _id: "9",
+            name: "Ripple Ice Cream",
+            image: food_9,
+            price: 16,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Deserts"
+        },
+        {
+            _id: "10",
+            name: "Fruit Ice Cream",
+            image: food_10,
+            price: 22,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Deserts"
+        },
+        {
+            _id: "11",
+            name: "Jor Ice Cream",
+            image: food_11,
+            price: 18,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Deserts"
+        },
+        {
+            _id: "12",
+            name: "Vanilla Ice Cream",
+            image: food_12,
+            price: 22,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Deserts"
+        },
+        {
+            _id: "13",
+            name: "Chicken Sandwich",
+            image: food_13,
+            price: 22,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Sandwich"
+        },
+        {
+            _id: "14",
+            name: "Vegon Sandwich",
+            image: food_14,
+            price: 22,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Sandwich"
+        },
+        {
+            _id: "15",
+            name: "Grilled Sandwich",
+            image: food_15,
+            price: 22,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Sandwich"
+        },
+        {
+            _id: "16",
+            name: "Bread Sandwich",
+            image: food_16,
+            price: 22,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Sandwich"
+        },
+           
+    ]
+    
